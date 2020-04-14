73c881e4b990490e454b7a1238b371da63f74b91
Ahmed Nusayer Ashik <42416199+AhmedNusayer@users.noreply.github.com>
2020-04-15 02:56:26 +0600

    Add files via upload

---
+++ Filling Diamonds.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 02:49:31 2020
+
+@author: User
+"""
+
+#Filling diamonds
+
+t = int(input())
+ans = []
+for i in range(t):
+    n = int(input())
+    ans.append(n)
+for j in range(len(ans)):
+    print(ans[j])
+
+
